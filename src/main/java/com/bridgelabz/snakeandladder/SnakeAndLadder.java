0c7fb6678baa94e89ba581450c5a7bbac2e1c187
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-06-24 10:25:43 +0530

    Added: Repeat till the Player reaches the winning position 100

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,25 +3,27 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("------Welcome to Snake and Ladder Problem------");
-        int startPosition = 0, currentPosition = 0;
-        System.out.println("Start position is: " + startPosition);
-        int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("Dice Number is : " + diceNumber);
-        int option = (int) (Math.floor(Math.random() * 10) % 3);
-        System.out.println("Option: " +option);
-        if(option == 0) {
-            System.out.println("No play");
-            currentPosition += 0;
-        } else if(option == 1) {
-            System.out.println("Ladder");
-            currentPosition += diceNumber;
-        } else {
-            System.out.println("Snake");
-            currentPosition -= diceNumber;
-            if(currentPosition < 0) {
-                currentPosition = startPosition;
+        int startPosition = 0, currentPosition = 0, finalPosition = 100;
+        currentPosition = startPosition;
+        while (currentPosition < finalPosition) {
+            int diceValue = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+            System.out.println("The player get dice value as: " + diceValue);
+            int option = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println("Option: " + option);
+            if (option == 0) {
+                System.out.println("No play");
+                currentPosition += 0;
+            } else if (option == 1) {
+                System.out.println("Ladder");
+                currentPosition += diceValue;
+            } else {
+                System.out.println("Snake");
+                currentPosition -= diceValue;
+                if (currentPosition < 0) {
+                    currentPosition = startPosition;
+                }
             }
+            System.out.println("Current Position: " + currentPosition);
         }
-        System.out.println("Current Position: " +currentPosition);
     }
 }
