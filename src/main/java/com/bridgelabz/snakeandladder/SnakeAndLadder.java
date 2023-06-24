29dad9a9702d63d58b3f5f0e0ae84ea3d0f4c9b1
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-06-24 10:19:22 +0530

    Added: The Player then checks for a Option. They are No Play, Ladder or Snake

--- src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,9 +3,25 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("------Welcome to Snake and Ladder Problem------");
-        int startPosition = 0;
-        System.out.println(" Start position is: " + startPosition);
+        int startPosition = 0, currentPosition = 0;
+        System.out.println("Start position is: " + startPosition);
         int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println(" Dice Number is : " + diceNumber);
+        System.out.println("Dice Number is : " + diceNumber);
+        int option = (int) (Math.floor(Math.random() * 10) % 3);
+        System.out.println("Option: " +option);
+        if(option == 0) {
+            System.out.println("No play");
+            currentPosition += 0;
+        } else if(option == 1) {
+            System.out.println("Ladder");
+            currentPosition += diceNumber;
+        } else {
+            System.out.println("Snake");
+            currentPosition -= diceNumber;
+            if(currentPosition < 0) {
+                currentPosition = startPosition;
+            }
+        }
+        System.out.println("Current Position: " +currentPosition);
     }
 }
